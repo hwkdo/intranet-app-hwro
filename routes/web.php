c36adf1a5ce48b7a613d1c499a42a7e60f45e585
aleex1848 <halycon01@gmail.com>
2026-08-28 12:13:59 +0000

    fix UserSettings

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,29 @@ use Hwkdo\IntranetAppHwro\Controllers\DokumentController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+
 Route::middleware(['web', 'auth', 'can:see-app-hwro'])->group(function () {
     Volt::route('apps/hwro', 'apps.hwro.index')->name('apps.hwro.index');
 
     // Vorgänge
     Volt::route('apps/hwro/vorgaenge', 'apps.hwro.vorgaenge.index')->name('apps.hwro.vorgaenge.index');
+
     Volt::route('apps/hwro/vorgaenge/create', 'apps.hwro.vorgaenge.create')->name('apps.hwro.vorgaenge.create');
+
     Volt::route('apps/hwro/vorgaenge/{vorgang}', 'apps.hwro.vorgaenge.show')->name('apps.hwro.vorgaenge.show');
 
     // Betriebsbesuche
     Volt::route('apps/hwro/betriebsbesuche', 'apps.hwro.betriebsbesuche.index')->name('apps.hwro.betriebsbesuche.index');
+
     Volt::route('apps/hwro/betriebsbesuche/{bnr}', 'apps.hwro.betriebsbesuche.show')->name('apps.hwro.betriebsbesuche.show');
 
     // Dokumente
     Volt::route('apps/hwro/dokumente', 'apps.hwro.dokumente.index')->name('apps.hwro.dokumente.index');
+
     Volt::route('apps/hwro/dokumente/create', 'apps.hwro.dokumente.create')->name('apps.hwro.dokumente.create');
+
     Volt::route('apps/hwro/dokumente/{dokument}', 'apps.hwro.dokumente.show')->name('apps.hwro.dokumente.show');
+
     Volt::route('apps/hwro/dokumente/{dokument}/edit', 'apps.hwro.dokumente.edit')->name('apps.hwro.dokumente.edit');
 
     // Dokument Download
@@ -38,7 +45,9 @@ Route::middleware(['web', 'auth', 'can:see-app-hwro'])->group(function () {
         ->name('apps.hwro.chat');
 });
 
+
 Route::middleware(['web','auth','can:manage-app-hwro'])->group(function () {
     Volt::route('apps/hwro/admin', 'apps.hwro.admin.index')->name('apps.hwro.admin.index');
+
     Route::redirect('apps/hwro/settings/admin', 'apps/hwro/admin');
 });
